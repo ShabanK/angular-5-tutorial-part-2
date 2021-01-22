6896149a28de2917a7b6868d26d13d0501c6145e
ShabanK <shabankhawar99@gmail.com>
2021-01-22 13:06:09 +0500

    statistics complete

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewsHomeComponent implements OnInit {
 
+  stats=[
+    {value:22, label: "# of Users"},
+    {value:900, label: "Revenue"},
+    {value:50, label:"Reviews"}
+  ]
+
   constructor() { }
 
   ngOnInit() {
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsHomeComponent } from './views-home/views-home.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { SharedModule } from "../shared/shared.module"
 
 @NgModule({
   imports: [
     CommonModule,
-    ViewsRoutingModule
+    ViewsRoutingModule,
+    SharedModule
   ],
-  declarations: [ViewsHomeComponent],
+  declarations: [ViewsHomeComponent, StatisticsComponent],
   exports:[]
 })
 export class ViewsModule { }
